9f1eda41141a703a844912e9aa4b25519873b7dc
MORI Shingo <cos@kmc.gr.jp>
2013-10-21 03:12:08 +0900

    add button

--- shinkei_suijaku/shinkeisuijaku.js
+++ shinkei_suijaku/shinkeisuijaku.js
@@ -1,13 +1,47 @@
+var w = 4;
+var h = 4;
 var card = [];
+
+function swap(a, b) {
+  var temp = a;
+  a = b;
+  b = temp;
+}
 
 function init(w, h) {
   var seq = [];
   for (var i = 0; i < w * h; i++) {
-    seq.push(int(i / 2));
+    seq.push(Math.floor(i / 2));
+  }
+  for (var i = 0; i < w * h; i++) {
+    var r = Math.floor(Math.random() * (w * h - i));
+    swap(seq[w * h - i - 1], seq[r]);
   }
   for (var y = 0; y < h; y++) {
     for (var x = 0; x < w; x++) {
+      // TODO 
     }
   }
 }
 
+function addButton(w, h) {
+  var objBody = document.body;
+  for (var y = 0; y < h; y++) {
+    for (var x = 0; x < w; x++) {
+      var button = document.createElement('button');
+      button.innerHTML = '?';
+      button.onClick = click(x, y);
+      objBody.appendChild(button);
+    }
+    var br = document.createElement('br');
+    objBody.appendChild(br);
+  }
+}
+
+function click(x, y) {
+  // TODO 
+}
+
+
+init(w, h);
+addButton(w, h);
