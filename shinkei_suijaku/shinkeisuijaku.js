9c60cb4b538d1950bfb30d58bd5e7af0f66a576b
MORI Shingo <cos@kmc.gr.jp>
2013-10-21 02:39:33 +0900

    add shinkei_suijaku skelton

---
+++ shinkei_suijaku/shinkeisuijaku.js
@@ -0,0 +1,13 @@
+var card = [];
+
+function init(w, h) {
+  var seq = [];
+  for (var i = 0; i < w * h; i++) {
+    seq.push(int(i / 2));
+  }
+  for (var y = 0; y < h; y++) {
+    for (var x = 0; x < w; x++) {
+    }
+  }
+}
+
