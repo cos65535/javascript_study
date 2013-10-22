3359caec6e6187fb3f8481e3e415cf7831f4cd01
MORI Shingo <cos@kmc.gr.jp>
2013-10-23 02:06:55 +0900

    we can do shinkei_suijaku
    However, we can't see the second card if we fail match.
    And, a clear time is not calculated.

--- shinkei_suijaku/shinkeisuijaku.js
+++ shinkei_suijaku/shinkeisuijaku.js
@@ -1,27 +1,48 @@
 var w = 4;
 var h = 4;
+var open_cnt = 0;
+var prev_x = -1;
+var prev_y = -1;
 var card = [];
 
-function swap(a, b) {
-  var temp = a;
-  a = b;
-  b = temp;
-}
+// bussy sleep
+function sleep( T ){ 
+  var d1 = new Date().getTime(); 
+  var d2 = new Date().getTime(); 
+  while( d2 < d1+1000*T ){    //T秒待つ 
+    d2=new Date().getTime(); 
+  } 
+  return; 
+} 
 
 function init(w, h) {
+  open_cnt = 0;
+  prev_x = -1;
+  prev_y = -1;
+  card = [];
   var seq = [];
   for (var i = 0; i < w * h; i++) {
     seq.push(Math.floor(i / 2));
+    console.log(seq[i]);
   }
   for (var i = 0; i < w * h; i++) {
     var r = Math.floor(Math.random() * (w * h - i));
-    swap(seq[w * h - i - 1], seq[r]);
+    var temp = seq[w * h - i - 1];
+    seq[w * h - i - 1] = seq[r];
+    seq[r] = temp;
+    console.log("r:" + r + ", i:" + (w * h - i - 1));
   }
   for (var y = 0; y < h; y++) {
+    card.push(new Array());
     for (var x = 0; x < w; x++) {
-      // TODO 
+      card[y].push(seq[y * w + x]);
+      // console.log(card[y][x]);
     }
   }
+}
+
+function getButtonId(x, y) {
+  return "button_" + x + "_" + y;
 }
 
 function addButton(w, h) {
@@ -30,7 +51,8 @@ function addButton(w, h) {
     for (var x = 0; x < w; x++) {
       var button = document.createElement('button');
       button.innerHTML = '?';
-      button.onClick = click(x, y);
+      button.id = getButtonId(x, y);
+      button.onclick = createClickCallback(x, y);
       objBody.appendChild(button);
     }
     var br = document.createElement('br');
@@ -38,8 +60,33 @@ function addButton(w, h) {
   }
 }
 
+function createClickCallback(x, y) {
+  return function() { click(x, y); }
+}
+
 function click(x, y) {
-  // TODO 
+  // console.log(getButtonId(x, y));
+  var button = document.getElementById(getButtonId(x, y));
+  if (button.innerHTML != "?") { return; }
+  button.innerHTML = card[y][x];
+  open_cnt++;
+  if (open_cnt % 2 == 0) {
+    var prev_button = document.getElementById(getButtonId(prev_x, prev_y));
+    if (card[y][x] == card[prev_y][prev_x]) {
+      // success
+    } else {
+      // fail
+      // TODO
+      button.innerHTML = "?";
+      prev_button.innerHTML = "?";
+      open_cnt -= 2;
+    }
+  } 
+  prev_x = x;
+  prev_y = y;
+  if (open_cnt == w * h) {
+    alert("Congratulations!");
+  }
 }
 
 
